c8679fb369128370399eb744e30ab276bcada037
kaveri_subramaniam_miq <kaveri.subramaniam@miqdigital.com>
2019-03-19 06:11:44 +0530

    UI has been hard coded for all except stu_dockerfile.html

--- src/server.py
+++ src/server.py
@@ -1,6 +1,3 @@
-# Decide whether to use SQLAlchemy (ORM) or MySQLdb (Might have to use a fork, MySQLDb does not have good python3 support)
-# For now going with pymysql as it is good enough and very similar to MySQLdb
-# Please follow the following naming convention: long_function_name(var_one,var_two)
 from flask import Flask, flash,session, render_template, request, redirect, Response ,jsonify, json, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from string import Template
@@ -31,10 +28,11 @@ def index():
 @app.route('/validate',methods=['POST'])
 def validate():
     data = json.loads(request.data)
+    print(data)
     usn = data["usn"]
     email = data["email"]
     pswd = data["password"]
-
+    role = data["role"]
     # cursor = db.cursor()
     # sql = "SELECT s_id, s_name, s_email, s_password from student where s_id = %s and s_email = %s"
     # args = ([usn, email])
@@ -45,13 +43,17 @@ def validate():
 
     results = [['01FB15EC', 'kaveri', 'kaveri.subra@gmail.com', 'kaveri']]
 
+
     if results:
         row = results[0]
 
         if(row[3] == pswd):
             # do session stuff
             session.clear()
-            session['s_id'] = row[0]
+            if(role == "student"):
+                session['s_id'] = row[0]
+            else:
+                session['p_id'] = row[0]
             session['name'] = row[1]
             return "Correct"
         else:
@@ -61,6 +63,21 @@ def validate():
     # If we still reach here, it means that the user is not a registered one
     return "Missing"
 
+#To logout
+@app.route('/logout')
+def logout():
+    if 's_id' in session:
+        session.pop('s_id')
+        return render_template('index.html')
+    elif 'p_id' in session:
+        session.pop('p_id')
+        return render_template('index.html')
+    else:
+        return render_template('index.html')
+
+#####
+# Main Page
+#####
 #To display profile page
 @app.route('/profile')
 def profile():
@@ -71,17 +88,53 @@ def profile():
     else:
         return render_template('index.html')
 
-#to get list fo assignments
-@app.route('/getList')
-def getList():
+#####
+# Prof Pages
+#####
+@app.route('/professor/get/<assignment_id>')
+def getAssignment(assignment_id):
+    if 'p_id' in session:
+        return render_template('prof_viewAssignment.html', name = session["name"])
+    else:
+        return render_template('index.html')
 
-    return 
+@app.route('/professor/create/assignment')
+def createAssigment():
+    if 'p_id' in session:
+        return render_template('prof_create.html', name = session["name"])
+    else:
+        return render_template('index.html')
 
-#To logout
-@app.route('/logout')
-def logout():
+@app.route('/professor/get/<assignment_id>/<submission_id>')
+def getSubmissions(assignment_id, submission_id):
+    if 'p_id' in session:
+        return render_template('prof_viewSubmission.html', name = session["name"])
+    else:
+        return render_template('index.html')
+
+####
+# Student Functions
+####
+
+@app.route('/student/get/<assignment_id>')
+def getSubmission(assignment_id):
     if 's_id' in session:
-        session.pop('s_id')
+        return render_template('stu_submission.html', name = session["name"])
+    else:
+        return render_template('index.html') 
+
+@app.route('/student/create/submission')
+def createSubmission():
+    if 's_id' in session:
+        return render_template('stu_submit.html', name = session["name"])
+    else:
+        return render_template('index.html')
+
+@app.route('/student/create/dockerfile')
+def createDockerfile():
+    if 's_id' in session:
+        return render_template('stu_dockerfile.html', name = session["name"])
+    else:
         return render_template('index.html')
 
 if __name__ == '__main__':
