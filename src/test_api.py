691d4b6577cfc516273a4eba94c5f201a26af177
kaveri_subramaniam_miqdigital <kaveri.subramaniam@miqdigital.com>
2019-04-24 15:52:30 +0530

    Removed Unwanted files

--- src/test_api.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, flash,session, render_template, request, redirect, Response ,jsonify, json, url_for
-from database import *
-
-app = Flask(__name__)
-app.secret_key = 'droplet'
-
-if __name__ == '__main__':
-# run!
-    app.run(debug=True)
